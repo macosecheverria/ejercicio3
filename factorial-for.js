fab5c93681115993859d4981c5a57e757a40f2dd
Marcos Echeverria <marcosdarkecheverria12@gmail.com>
2022-11-01 18:15:27 -0300

    ejercicio3 open bootcamp

---
+++ factorial-for.js
@@ -0,0 +1,10 @@
+//factorial for 
+
+
+let numero = 10;
+let resultado = 1;
+
+for(let i =1;i<=numero;i++){
+    resultado *= i;
+    console.log(resultado);
+}
---
+++ factorial-while.js
@@ -0,0 +1,16 @@
+//con bucle while
+
+let numero2 = 10;
+let resultado2 = 1;
+let i = 1;
+
+while(i <= numero2){
+    resultado2 *= i;
+    console.log(resultado2);
+    i++;
+    
+}
+
+
+
+
